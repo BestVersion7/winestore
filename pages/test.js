6845ccc0a01f572e0265966950c5d5c0f6d284d3
BestVersion7 <hunterkfan@gmail.com>
2022-06-04 00:38:26 -0400

    test3

--- pages/test.js
+++ pages/test.js
@@ -1,16 +1,28 @@
 import ReactMarkdown from "react-markdown";
+import prisma from "../lib/prisma";
 
-const TestPage = () => {
+const TestPage = ({ data }) => {
     return (
         <div>
-            <ReactMarkdown>
-                ### Love
-                [Retool](https://retool.com) Man, imagine how *annoying* it
-                would be to have to write **all** of this using HTML tags
-            </ReactMarkdown>
-            ,
+            <ReactMarkdown>{data.article_post}</ReactMarkdown>,
         </div>
     );
 };
 
 export default TestPage;
+
+export const getStaticProps = async (req, res) => {
+    const data2 = await prisma.blog.findFirst({
+        where: {
+            article_id: 1,
+        },
+        select: {
+            article_post: true,
+        },
+    });
+    const data = JSON.parse(JSON.stringify(data2));
+    console.log(data);
+    return {
+        props: { data },
+    };
+};
